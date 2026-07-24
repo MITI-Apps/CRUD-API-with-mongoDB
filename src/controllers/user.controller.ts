8ba292d46ce7bea6019f536f27175a730ec59b5e
Okeke Miracle <okekem186@gmail.com>
2026-07-24 01:29:15 +0100

    learned how to hash password

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,9 +1,10 @@
 import User from "../models/user.module.js";
 import type { Request, Response } from "express";
+import bcrypt from "bcrypt";
 
 const getAllUsers = async (req: Request, res: Response) => {
   try {
-    const users = await User.find();
+    const users = await User.find().select("-password"); // Exclude password field from the response
     res.status(200).json(users);
   } catch (error) {
     res.status(500).json({ error: "Failed to fetch users" });
@@ -12,7 +13,7 @@ const getAllUsers = async (req: Request, res: Response) => {
 
 const getUserById = async (req: Request, res: Response) => {
   try {
-    const user = await User.findById(req.params.id);
+    const user = await User.findById(req.params.id).select("-password"); // Exclude password field from the response
     if (!user) {
       return res.status(404).json({ error: "User not found" });
     }
@@ -24,7 +25,9 @@ const getUserById = async (req: Request, res: Response) => {
 
 const createUser = async (req: Request, res: Response) => {
   try {
-    const user = new User(req.body);
+    const { name, email, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new User({ ...req.body, password: hashedPassword });
     await user.save();
     res.status(201).json(user);
   } catch (error) {
@@ -32,30 +35,6 @@ const createUser = async (req: Request, res: Response) => {
   }
 };
 
-const updateUser = async (req: Request, res: Response) => {
-  try {
-    const { id } = req.params;
-    const user = await User.findByIdAndUpdate(id, req.body, { new: true });
-    if (!user) {
-      return res.status(404).json({ error: "User not found" });
-    }
-    res.status(200).json(user);
-  } catch (error) {
-    res.status(500).json({ error: "Failed to update user" });
-  }
-};
 
-const deleteUser = async (req: Request, res: Response) => {
-  try {
-    const id = req.params.id;
-    const user = await User.findByIdAndDelete(id);
-    if (!user) {
-      return res.status(404).json({ error: "User not found" });
-    }
-    res.status(200).json({ message: "User deleted successfully" });
-  } catch (error) {
-    res.status(500).json({ error: "Failed to delete user" });
-  }
-};
 
-export { getAllUsers, getUserById, createUser, updateUser, deleteUser };
+export { createUser, getAllUsers, getUserById };
--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,15 +1,12 @@
 import express from "express";
 const router = express.Router();
-import { getAllUsers, getUserById, createUser, updateUser, deleteUser } from "../controllers/user.controller.js";
+import { createUser, getAllUsers, getUserById } from "../controllers/user.controller.js";
+
+
+router.post("/", createUser);
 
 router.get("/", getAllUsers);
 
 router.get("/:id", getUserById);
-
-router.post("/", createUser);
-
-router.put("/:id", updateUser);
-
-router.delete("/:id", deleteUser);
 
 export default router;
